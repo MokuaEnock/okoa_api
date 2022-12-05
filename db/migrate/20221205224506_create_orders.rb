599dfc9f134a38929cf4fe10b8835c292e9aa8b7
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 01:58:20 +0300

    create attributes for user and order

--- db/migrate/20221205224506_create_orders.rb
+++ db/migrate/20221205224506_create_orders.rb
@@ -1,6 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
+      t.belongs_to :user, foreign_key: user_id
+      t.string :name
+      t.string :location
+      t.integer :weight
+      t.string :type
+      t.string :instructions
+      t.date :date
 
       t.timestamps
     end
