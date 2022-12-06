cc43ed70cb42ba4ec604404195cd55026a14a420
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 11:36:42 +0300

    refactor code for user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,9 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
 
   def create
-    user = User.create(user_params)
-    if user.valid?
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: {
-               errors: user.errors.full_messages
-             },
-             status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
   end
 
   def show
