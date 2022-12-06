883f3643796cd25a02fd8ec82e9b4aeff5f8ae89
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 20:37:38 +0300

    create show action for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  skip_before_action :authorize, only: :index
+
   def index
     render json: Order.all
   end
@@ -6,6 +8,11 @@ class OrdersController < ApplicationController
   def create
     recipe = @current_user.orders.create!(order_params)
     render json: recipe, status: :created
+  end
+
+  def show
+    recipe = Order.find(params[:id])
+    render json: recipe, status: :ok
   end
 
   private
