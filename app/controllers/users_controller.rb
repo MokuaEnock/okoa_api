43f32a057fdcafe18dd996c5f6d71a93dffcd75b
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 03:06:29 +0300

    create error handling for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity
+
   def create
     user = User.create!(user_params)
   end
 
   private
 
+  #error handling
+  def render_uprocessable_entity(invalid)
+    render json: { error: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  #params
   def user_params
     params.permit(:username, :password)
   end
