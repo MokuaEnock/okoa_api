2c42e83bdc9b1658dc75cee9675ec015cd052261
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 03:26:28 +0300

    create login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+    user = user.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      render json: user, status: :created
+    else
+      render json: {
+               errors: {
+                 login: "Invalid username or password"
+               }
+             },
+             status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :users
+  resources :users, only[:show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/login", to: "session#create"
 end
