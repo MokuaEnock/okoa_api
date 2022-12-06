92701678557a531f577c348ed41f50029d19e9f8
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 03:02:10 +0300

    add user params to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def create
+    user = User.create!(user_params)
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
   end
 end
