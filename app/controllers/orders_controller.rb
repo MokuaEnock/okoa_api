3830d55ff6896b2d7cafeb15616fddc5e78d2378
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 04:24:14 +0300

    add create action and order params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,15 @@ class OrdersController < ApplicationController
   def index
     render json: Order.all
   end
+
+  def create
+    recipe = @current_user.orders.create!(order_params)
+    redner json: recipe, status: :created
+  end
+
+  private
+
+  def order_params
+    params.permit(:name, :location, :weight, :type, :instructions, :date)
+  end
 end
