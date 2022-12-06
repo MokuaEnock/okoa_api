98ae4f73d6b0376f3e93c09e27c7ec459aa3876f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 03:29:34 +0300

    create authorized function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorized
 
-  def 
+  def authorized
+    unless session.include? :user_id
+      return render json: { error: "Not Authorized" }, status: :unauthorized
+    end
+  end
 end
