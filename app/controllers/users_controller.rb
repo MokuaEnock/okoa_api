ae6b05c8927d8e1e2ab4d5de21843987b7b504ad
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 04:14:43 +0300

    remove reduddant code in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   wrap_parameters format: []
-  rescue_from ActiveRecord::RecordInvalid, with: :render_uprocessable_entity
 
   def create
     user = User.create!(user_params)
@@ -12,11 +11,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  #error handling
-  def render_uprocessable_entity(invalid)
-    render json: { error: invalid.record.errors }, status: :unprocessable_entity
-  end
 
   #params
   def user_params
