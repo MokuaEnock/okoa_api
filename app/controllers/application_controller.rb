4f6e85df54ac5c9766b6c8e50715125b62f5ced4
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 04:10:41 +0300

    add before_action and rescue errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  private
+
   def authorize
     @current_user = User.find_by(id: session[:user_id])
     unless session.include? :user_id
@@ -14,5 +21,4 @@ class ApplicationController < ActionController::API
            },
            status: :unprocessable_entity
   end
-  
 end
