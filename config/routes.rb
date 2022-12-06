718fbc9d661b8e088ef5a346e457113c2208e904
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-06 04:06:08 +0300

    create required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :users, only[:show, :create]
+  resources :orders, only: %i[index create show update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/login", to: "session#create"
-  get "/me", to: "user#show"
-  delete "/logout", to: "user#destroy"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "users#destroy"
 end
