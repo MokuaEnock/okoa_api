395baaf67641b481f7e0d5c2bde46e5a3329687f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-07 21:52:00 +0300

    validations for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
+  validates :instructions, presence: true, length: { minimum: 16, maximum: 200 }
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :location, presence: true, length: { minimum: 4 }
+  validates :wastetype, presence: true
+  validates :weight, presence: true
+  validates :date, presence: true
+
   belongs_to :user
 end
